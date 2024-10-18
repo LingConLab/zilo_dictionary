7964573513dff15c5f11da77b5399f6bc7c2ac87
George Moroz <agricolamz@gmail.com>
2024-10-18 10:56:15 +0300

    update

---
+++ character_map.js
@@ -0,0 +1,58 @@
+function(settings) {
+var instance = settings.oInstance;
+var table = instance.api();
+
+const characterMaps = [
+{
+base: "I", // latin uppercase I, // то что вписываем
+letters: ["04C0","04CF","007C"] // palochka or vertical line // на что заменяем
+},
+{
+  base: "1",
+  letters: ["04C0","04CF","007C"]
+}
+
+,
+{
+  base: "!",
+  letters: ["04C0","04CF","007C"]
+}
+,
+{
+  base: "н", // cyrillic En
+  letters: ["1D34"] // "Modifier Letter Capital H"
+}
+];
+
+const replaceSpecificCharacters = function (s) {
+for (let i = 0; i < characterMaps.length; i++) {
+s = s.replace(characterMaps[i].base, ConvertToCharFromUnicode(characterMaps[i]));
+}
+return s;
+};
+
+// на что мы хотим заменить base, т.е. то, что может быть в инпуте
+const ConvertToCharFromUnicode = function (characterMap) {
+const letters = characterMap.letters;
+//const splitted = letters.split(","); // можно заменить на массив строк в словаре и удалить вот это разделение
+
+
+const resultAsChars = [];
+for (let i = 0; i < letters.length; i++) {
+resultAsChars.push(String.fromCharCode(parseInt(letters[i], 16)));
+}
+const result = "[" + characterMap.base + resultAsChars.join("") + "]"; // regex собирается здесь // .base позволяет тут искать и по тому символу, что вбивали
+
+return result;
+};
+
+var $inputs = instance.parent().find('.form-group input');
+$inputs.off('keyup search input').on('keyup', function () {
+  
+var index = $inputs.index(this); // I turned off rownames; column index starts from 0; if rownames are ON add +1
+
+
+let keyword = replaceSpecificCharacters($(this).val());
+table.column(index).search(keyword, true, false).draw();
+});
+}

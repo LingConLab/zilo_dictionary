a7e565769c14b8abfbfc2f407bcdadce154cd6ce
George Moroz <agricolamz@gmail.com>
2024-10-18 11:05:50 +0300

    update

--- character_map.js
+++ character_map.js
@@ -1,11 +1,11 @@
 function(settings) {
-var instance = settings.oInstance;
-var table = instance.api();
+  var instance = settings.oInstance;
+  var table = instance.api();
 
 const characterMaps = [
 {
-base: "I", // latin uppercase I, // то что вписываем
-letters: ["04C0","04CF","007C"] // palochka or vertical line // на что заменяем
+  base: "I", // latin uppercase I, // то что вписываем
+  letters: ["04C0","04CF","007C"] // palochka or vertical line // на что заменяем
 },
 {
   base: "1",
@@ -17,18 +17,13 @@ letters: ["04C0","04CF","007C"] // palochka or vertical line // на что за
   base: "!",
   letters: ["04C0","04CF","007C"]
 }
-,
-{
-  base: "н", // cyrillic En
-  letters: ["1D34"] // "Modifier Letter Capital H"
-}
 ];
 
 const replaceSpecificCharacters = function (s) {
-for (let i = 0; i < characterMaps.length; i++) {
-s = s.replace(characterMaps[i].base, ConvertToCharFromUnicode(characterMaps[i]));
-}
-return s;
+  for (let i = 0; i < characterMaps.length; i++) {
+    s = s.replace(characterMaps[i].base, ConvertToCharFromUnicode(characterMaps[i]));
+  }
+  return s;
 };
 
 // на что мы хотим заменить base, т.е. то, что может быть в инпуте
@@ -36,10 +31,9 @@ const ConvertToCharFromUnicode = function (characterMap) {
 const letters = characterMap.letters;
 //const splitted = letters.split(","); // можно заменить на массив строк в словаре и удалить вот это разделение
 
-
 const resultAsChars = [];
 for (let i = 0; i < letters.length; i++) {
-resultAsChars.push(String.fromCharCode(parseInt(letters[i], 16)));
+  resultAsChars.push(String.fromCharCode(parseInt(letters[i], 16)));
 }
 const result = "[" + characterMap.base + resultAsChars.join("") + "]"; // regex собирается здесь // .base позволяет тут искать и по тому символу, что вбивали
 
